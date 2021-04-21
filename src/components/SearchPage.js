4c739cecef80d0b2f7a336da95c55d002002e62b
Awesome Henry <henryebomah@gmail.com>
2021-04-21 11:58:59 -0700

    Add files via upload

---
+++ src/components/SearchPage.js
@@ -0,0 +1,90 @@
+import "./SearchPage.css";
+import React, { useState, useEffect } from "react";
+import Form from "./Form";
+
+import Todolist from "./Todolist";
+
+function SearchPage() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus]= useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  
+  //run once when app starts
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+  //use effect
+  useEffect(() => {
+    // eslint-disable-next-line
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+        case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+  };
+  //save to local
+  const saveLocalTodos = () => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+  };
+  const getLocalTodos = () => {
+    if(localStorage.getItem('todos') === null){
+      localStorage.setItem('todos', JSON.stringify([]));
+    }else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'))
+    setTodos(todoLocal);
+    }
+  };
+
+  return (
+    <div className="SearchPage">
+      <header>
+      <h1>Weekly Tasks</h1>
+      </header>
+      <Form 
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText}
+      inputText={inputText}
+      setStatus={setStatus}
+      />
+      <Todolist filteredTodos={filteredTodos} setTodos={setTodos} todos={todos}/>
+      <section className="section section--three u-margin-bottom-xxxl">
+
+</section>
+
+<footer className="footer u-margin-bottom-big">
+  <center><div className="footer__brand-container">
+    <p className="footer__author-text">
+      Created by <a href="http://www.henryebomah-awesomething.vercel.app/" target="_blank" rel="noreferrer">Henry Ebomah</a>
+    </p>
+    <div className="footer__social-media u-margin-bottom-small ">
+      <a href="https://github.com/awesomething" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
+      |
+      <a href="https://www.linkedin.com/in/henryebomah/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a>
+    </div>
+    <div className="copyright-text">
+      <p>
+        Copyright &copy; 2021
+      <br />
+        All Rights Reserved
+    </p>
+    </div>
+  </div></center>
+</footer>
+    </div>
+  );
+}
+
+export default SearchPage;
